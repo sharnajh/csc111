a5e37009c610debfb9b7dc87ffa5609ebe9b49e6
Sharna Hossain <sharnajh@gmail.com>
2020-10-16 00:56:09 -0400

    dsdj

---
+++ lab10/average_for_loop.cpp
@@ -0,0 +1,41 @@
+// Sharna Hossain
+// CSC 111
+// Lab 10 | average_for_loop.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sum, input;
+    const int MIN = 0, MAX = 100;
+    const double TOTAL = 3.0;
+    double average;
+    char again;
+
+    do
+    {
+        sum = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            cout << "Enter a grade from 0-100: ";
+            cin >> input;
+            while (!(input >= MIN) || !(input <= MAX))
+            {
+                cout << "ERROR: Invalid grade value!\n";
+                cout << "Enter a grade from 0-100: ";
+                cin >> input;
+            }
+            sum += input;
+        }
+
+        average = sum / TOTAL;
+        cout << "The average is " << average << endl;
+
+        cout << "Do you want to average new grades? (Y/N) \n";
+        cin >> again;
+
+    } while (again == 'y' || again == 'Y');
+
+    return 0;
+}
