8c84f0cdea75f8f6c3fdc976ac3b6a71ca0eb4b2
Sharna Hossain <sharnajh@gmail.com>
2020-11-03 14:50:30 -0500

    fjsldfk

--- lab13/rectangles.cpp
+++ lab13/rectangles.cpp
@@ -16,14 +16,18 @@ double Area(double l, double w)
 int main()
 {
     double l, w, perimeter, area;
+
     cout << "Enter the width: ";
     cin >> w;
     cout << "Enter the length: ";
     cin >> l;
+
     perimeter = Perimeter(l, w);
     area = Area(l, w);
+
     cout << "The perimeter is " << perimeter << "\n";
     cout << "The area is " << area << "\n";
+    
     return 0;
 }
 
