353115002ade9a221e860c9bf0c284ca0393385d
Sharna Hossain <sharnajh@gmail.com>
2020-11-04 23:50:10 -0500

    fdsfsd

--- lab15/findmax.cpp
+++ lab15/findmax.cpp
@@ -9,19 +9,37 @@
 #include <iostream>
 using namespace std;
 
-int FindMax(int num1, int num2)
+// int FindMax(int num1, int num2)
+// {
+//     return num1 > num2 ? num1 : num2;
+// }
+
+// int main()
+// {
+//     int num1, num2;
+
+//     cout << "Enter 2 numbers: ";
+//     cin >> num1 >> num2;
+
+//     cout << "Max number is " << FindMax(num1, num2) << endl;
+
+//     return 0;
+// }
+
+int FindMax(int arr[], int length)
 {
-    return num1 > num2 ? num1 : num2;
+    int max = arr[0];
+    for (int i = 0; i < length; i++)
+    {
+        max = max > arr[i] ? max : arr[i];
+    }
+    return max;
 }
 
 int main()
 {
-    int num1, num2;
-
-    cout << "Enter 2 numbers: ";
-    cin >> num1 >> num2;
-
-    cout << "Max number is " << FindMax(num1, num2) << endl;
-
+    int arr[] = {-10, 20, -100, 400, 30, 5, 900};
+    int length = sizeof(arr) / sizeof(*arr);
+    cout << "Max number is " << FindMax(arr, length) << endl;
     return 0;
 }
