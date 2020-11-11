7d95e767242e4dc8e9ae3edf88ec54e9ccc8a526
Sharna Hossain <sharnajh@gmail.com>
2020-11-10 21:28:09 -0500

    fsjdfj

---
+++ lab17/max_min_arrs.cpp
@@ -0,0 +1,34 @@
+// Sharna Hossain
+// CSC 111
+// Lab 17 | max_min_arrs.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size, min, max;
+
+    cout << "How many numbers do you want to enter? ";
+    cin >> size;
+
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Please enter number " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    max = arr[0], min = arr[0];
+    for (int j = 0; j < size; j++)
+    {
+        max = max > arr[j] ? max : arr[j];
+        min = min < arr[j] ? min : arr[j];
+    }
+
+    cout << "Among the numbers you entered the ";
+    cout << "Max is " << max << " and the ";
+    cout << "Min is " << min << endl;
+
+    return 0;
+}
