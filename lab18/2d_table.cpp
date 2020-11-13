5c71ee18424705e2bc3d1560de0819fcb5656403
Sharna Hossain <sharnajh@gmail.com>
2020-11-13 10:22:23 -0500

    djskdf

--- lab18/2d_table.cpp
+++ lab18/2d_table.cpp
@@ -24,6 +24,15 @@ int main()
         }
     }
 
+    // for (int r = 0; r <= rows; r++)
+    // {
+    //     for (int c = 0; c <= columns; c++)
+    //     {
+    //         cout << setw(digits) << arr[r][c];
+    //     }
+    //     cout << "\n";
+    // }
+
     // Output values
     // Column Header
     cout << setw(digits) << " ";
--- lab18/tempCodeRunnerFile.cpp
+++ lab18/tempCodeRunnerFile.cpp
@@ -1,18 +1,8 @@
-    // // Column Header
-    // cout << setw(digits) << " ";
-    // for (int c = 1; c <= columns; c++)
+   // for (int r = 0; r <= rows; r++)
     // {
-    //     cout << setw(digits) << c;
-    // }
-    // cout << endl;
-
-    // for (int r = 1; r <= rows; r++)
-    // {
-    //     // Row Header
-    //     cout << setw(digits) << r;
-    //     for (int c = 1; c <= columns; c++)
+    //     for (int c = 0; c <= columns; c++)
     //     {
     //         cout << setw(digits) << arr[r][c];
     //     }
-    //     cout << endl;
+    //     cout << "\n";
     // }
