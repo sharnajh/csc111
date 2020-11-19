ec4a3b10648684dfaa4eb92895b233c315ab6c26
Sharna Hossain <sharnajh@gmail.com>
2020-11-19 15:15:18 -0500

    fsjld

--- lab20/linear_search.cpp
+++ lab20/linear_search.cpp
@@ -11,10 +11,7 @@ int search(int arr[], int size, int target)
 {
     for (int i = 0; i < size; i++)
     {
-        if (arr[i] == target)
-        {
-            return i;
-        }
+        if (arr[i] == target) return i;
     }
     return -1;
 }
