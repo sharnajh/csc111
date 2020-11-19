285a2930597ff7c070970f5457bcf498059275fc
Sharna Hossain <sharnajh@gmail.com>
2020-11-19 13:19:24 -0500

    fsdf

--- lab18/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-   // for (int r = 0; r <= rows; r++)
-    // {
-    //     for (int c = 0; c <= columns; c++)
-    //     {
-    //         cout << setw(digits) << arr[r][c];
-    //     }
-    //     cout << "\n";
-    // }
