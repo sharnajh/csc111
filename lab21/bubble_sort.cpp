c16f0be0f3232a548190c7fb1bba4ba64a3b132b
Sharna Hossain <sharnajh@gmail.com>
2020-11-25 03:05:41 -0500

    dsfjdl

--- lab21/bubble_sort.cpp
+++ lab21/bubble_sort.cpp
@@ -16,16 +16,16 @@ void bubble_sort(int arr[], int size)
         swapped = false;
         for (int i = 0; i < size - 1; i++)
         {
+            
             for (int j = 0; j < size - i - 1; j++)
             {
                 if (arr[j] > arr[j + 1])
                 {
                     // temp vars
-                    int x = arr[j];
-                    int y = arr[j + 1];
+                    int temp = arr[j];
 
-                    arr[j + 1] = x;
-                    arr[j] = y;
+                    arr[j] = arr[j+1];
+                    arr[j + 1] = temp;
 
                     swapped = true;
                 }
--- lab21/selection_sort.cpp
+++ lab21/selection_sort.cpp
@@ -3,9 +3,60 @@
 // Lab 21 | selection_sort.cpp
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void output(int[], int);
+
+void selection_sort(int arr[], int size)
+{
+    int min;
+    for (int i = 0; i < size; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[min] > arr[j])
+            {
+                min = j;
+            }
+        }
+        if (min != i) {
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Output values
+    output(arr, size);
+}
 
 int main()
 {
+    // Initialize values
+    int size;
+    cout << "Enter the size of your list: ";
+    cin >> size;
+
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter element #" << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    // Sort and output
+    selection_sort(arr, size);
+
     return 0;
+}
+
+void output(int arr[], int size)
+{
+    cout << "Here is your selection-sorted list of numbers:\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << setw(4) << arr[i];
+    }
 }
