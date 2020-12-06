de8fd5142b66708e232f627bcaaa4336604725c4
Sharna Hossain <sharnajh@gmail.com>
2020-12-05 19:57:50 -0500

    Fsdfj

---
+++ lab23/lab23.cpp
@@ -0,0 +1,71 @@
+// Sharna Hossain
+// CSC 111
+// Lab 23 | lab23.cpp
+
+// First, define the rectangle class which
+// has two functions for calculating area
+// and perimeter. then write a c++ program
+// that in its main function has 2 square
+// objects and calculate their area and
+// perimeter. for each object ask the user
+// to give you the sides.
+
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+private:
+    double width;
+    double length;
+
+public:
+    void set_width(double);
+    void set_length(double);
+    double calc_area() const;
+    double calc_perimeter() const;
+};
+
+int main()
+{
+    for (int i = 1; i <= 2; i++)
+    {
+        Rectangle shape;
+        double width, length;
+
+        cout << "Enter Rectangle #" << i << " length: ";
+        cin >> length;
+        cout << "Enter Rectangle #" << i << " width: ";
+        cin >> width;
+
+        shape.set_width(width);
+        shape.set_length(length);
+
+        cout << "Rectangle #" << i << " area: "
+             << shape.calc_area() << "\n";
+        cout << "Rectangle #" << i << " perimeter: "
+             << shape.calc_perimeter() << "\n";
+    }
+
+    return 0;
+}
+
+void Rectangle::set_width(double w)
+{
+    width = w;
+};
+
+void Rectangle::set_length(double l)
+{
+    length = l;
+};
+
+double Rectangle::calc_area() const
+{
+    return width * length;
+};
+
+double Rectangle::calc_perimeter() const
+{
+    return 2.0 * (width + length);
+};
